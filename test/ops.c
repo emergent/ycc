c59e1ab737626470822d45d36e76cbf67b02909c
emergent <yosikawa@gmail.com>
2018-05-08 04:44:05 +0900

    edit test code

--- test/ops.c
+++ test/ops.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+void add(int* x) {
+    *     x = 100;
+}
+
 int main() {
     // number
     int x = 1;
@@ -10,9 +15,31 @@ int main() {
     z = z - (-1);
     double a = z / 1;
     float b = z / 100L;
+    y = 0;
+    y = + 1 ;
+    printf("y = %d\n", y);
+    y  ++;
+    printf("y = %d\n", y);
+    ++     y;
+    printf("y = %d\n", y);
+    y   --;
+    printf("y = %d\n", y);
+    --  y;
+    printf("y = %d\n", y);
+    y += - - -1;
+    printf("y = %d\n", y);
+    y = y > 0 ? 5 : 2;
+    printf("y = %d\n", y);
+    y = y | 1;
+    printf("y = %d\n", y);
+    y = 7 & y;
+    printf("y = %d\n", y);
+    
+
+    add( & x);
+    printf("x = %d\n", x);
 
     printf("%f\n", a+b);
-
 
     // this is comment
     /* this is
